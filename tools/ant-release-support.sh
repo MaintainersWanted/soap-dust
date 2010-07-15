fd0cd60043d6acd224adde43e670ee94246329a5
pascal <pascal@b1201410-bbdb-43d7-8ed3-e26efeef9164>
2010-07-15 13:50:36 +0000

    test svn
    git-svn-id: svn://svn.code.sf.net/p/soap-dust/code/soap-dust/0.0@39 b1201410-bbdb-43d7-8ed3-e26efeef9164

---
+++ tools/ant-release-support.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+svn up || exit $?
+
+if [ ! -z "$(svn st)" ]
+then
+  echo "You have local modifications you need to check in svn before proceeding" >&2
+  exit 1
+fi
+
+DELIVERABLE=$(LANG=C svn info . | grep URL | sed -e 's:.*/\(.*\)/\(.*\):\1-\2:')
+
+REVISION=$(LANG=C svn info . | grep Revision | cut -d' ' -f2)
+
+echo $DELIVERABLE.$REVISION
