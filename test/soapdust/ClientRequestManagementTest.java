3a9ad9de1f3b6625abea8ff9309652cb448de41b
pascal <pascal@b1201410-bbdb-43d7-8ed3-e26efeef9164>
2011-05-09 20:59:31 +0000

    FIX tests : old FIXMEs finally fixed and login/password of jira test becomes what it always should have been : in0/in1 (so cool...)
    git-svn-id: svn://svn.code.sf.net/p/soap-dust/code/soap-dust/0.1@115 b1201410-bbdb-43d7-8ed3-e26efeef9164

--- test/soapdust/ClientRequestManagementTest.java
+++ test/soapdust/ClientRequestManagementTest.java
@@ -7,6 +7,8 @@ import soapdust.urlhandler.test.Handler;
 
 public class ClientRequestManagementTest extends TestCase {
 
+	//FIXME distinguish between literal and encoded...
+	
     // the 2 following tests are slow... because jira.wsdl takes time to parse
     public void testBuildXmlSoapJiraRequest() throws IOException, MalformedWsdlException, FaultResponseException, MalformedResponseException {
         Client client = new Client();
@@ -14,8 +16,8 @@ public class ClientRequestManagementTest extends TestCase {
         client.setEndPoint("test:file:test/soapdust/response-with-href.xml");//TODO add a response.xml file for general purpose queries
 
         ComposedValue authentication = new ComposedValue();
-        authentication.put("login", "login");
-        authentication.put("password", "password");
+        authentication.put("in0", "login");
+        authentication.put("in1", "password");
 
         client.call("login", authentication);
 
@@ -26,8 +28,8 @@ public class ClientRequestManagementTest extends TestCase {
             "<Header/>" +
             "<Body>" +
             "<login xmlns=\"http://jira.codehaus.org//rpc/soap/jirasoapservice-v2\">" +
-            "<login>login</login>" +
-            "<password>password</password>" +
+            "<in0>login</in0>" +
+            "<in1>password</in1>" +
             "</login>" +
             "</Body>" +
             "</Envelope>";
@@ -69,11 +71,11 @@ public class ClientRequestManagementTest extends TestCase {
             "<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
             "<Header/>" +
             "<Body>" +
-            "<messageParameter1Element xmlns=\"definitionNS\">" + //FIXME should be xmlns=\"Element1NS\">
-              "<sender xmlns=\"element1NS\">sender</sender>" +
-              "<MSISDN xmlns=\"element1NS\">30123456789</MSISDN>" +
-              "<IDOffre xmlns=\"element1NS\">12043</IDOffre>" +
-              "<doscli xmlns=\"element1NS\">" +
+            "<messageParameter1Element xmlns=\"element1NS\">" +
+              "<sender>sender</sender>" +
+              "<MSISDN>30123456789</MSISDN>" +
+              "<IDOffre>12043</IDOffre>" +
+              "<doscli>" +
                 "<subParameter1 xmlns=\"schema1NS\">1</subParameter1>" +
                 "<subParameter2 xmlns=\"schema1NS\">2</subParameter2>" +
                 "<subParameter3 xmlns=\"schema1NS\">3</subParameter3>" +
@@ -121,11 +123,11 @@ public class ClientRequestManagementTest extends TestCase {
             "<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
             "<Header/>" +
             "<Body>" +
-            "<messageParameter1Element xmlns=\"definitionNS\">" + //FIXME should be xmlns=\"Element1NS\">
-            "<sender xmlns=\"element1NS\">sender</sender>" +
-            "<MSISDN xmlns=\"element1NS\">30123456789</MSISDN>" +
-            "<IDOffre xmlns=\"element1NS\">12043</IDOffre>" +
-            "<doscli xmlns:ns0=\"http://www.w3.org/2001/XMLSchema-instance\" ns0:type=\"messageSubSubParameter\" xmlns=\"element1NS\">" +
+            "<messageParameter1Element xmlns=\"element1NS\">" + 
+            "<sender>sender</sender>" +
+            "<MSISDN>30123456789</MSISDN>" +
+            "<IDOffre>12043</IDOffre>" +
+            "<doscli xmlns:ns0=\"http://www.w3.org/2001/XMLSchema-instance\" ns0:type=\"messageSubSubParameter\">" +
               "<subParameter1 xmlns=\"schema1NS\">1</subParameter1>" +
               "<subParameter2 xmlns=\"schema1NS\">2</subParameter2>" +
               "<subParameter3 xmlns=\"schema1NS\">3</subParameter3>" +
