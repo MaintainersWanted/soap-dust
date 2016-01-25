365cf23e7562a1be677ae433fb1379e03a91789a
Pascal Grange <pascal@grange.nom.fr>
2016-01-25 15:48:48 +0100

    FIX do not index index.html...

--- tools/publish-doc.sh
+++ tools/publish-doc.sh
@@ -10,7 +10,7 @@ git checkout gh-pages
 git pull origin gh-pages
 cp -r target/api "api/$branch"
 
-ls api \
+ls api | grep -v index.html \
 | perl -e 'print "<html><body><ul>"; while(<>) { chop $_;  print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > api/index.html
 
 git add "api"
